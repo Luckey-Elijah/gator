8a1085000f5840773a2c142317f61f06a7c4a91e
Elijah Luckey <luckeyelijah112@gmail.com>
2023-06-23 12:41:21 -0400

    updates asserts

--- lib/utils/asserts.dart
+++ lib/utils/asserts.dart
@@ -1,15 +1,50 @@
 /// Assert that the [factor] value is somewhere from `0` - `100`.
+/// Will throw [InvalidValueRangeException] if not in range.
 void assertFactor(int factor) {
-  assert(
-    factor > -1 && factor < 101,
-    '[factor] must be between -1 and 101',
+  const range = (lower: 0, upper: 100);
+  if (_inRange(range: range, value: factor)) return;
+  throw InvalidValueRangeException(
+    value: factor,
+    range: range,
   );
 }
 
-/// Assert that the color value is somewhere from `0` - `255`
+/// Assert that the color value is somewhere from `0` - `255`.
+/// Will throw [InvalidValueRangeException] if not in range.
 void assertValue(int value) {
-  assert(
-    value > -1 && value < 256,
-    '[value] must be between -1 and 256',
-  );
+  const range = (lower: 0, upper: 255);
+  if (_inRange(range: range, value: value)) return;
+  throw InvalidValueRangeException(value: value, range: range);
 }
+
+/// {@template invalid_value_range_exception}
+/// Exception for invalid integer ranges.
+/// {@endtemplate}
+class InvalidValueRangeException implements Exception {
+  /// {@macro invalid_value_range_exception}
+  InvalidValueRangeException({
+    required this.range,
+    required this.value,
+  });
+
+  /// Lower and upper limits of the allowed values.
+  final ({num lower, num upper}) range;
+
+  /// The actual value.
+  final num value;
+
+  /// Description of the exception.
+  String get message => '$value is not in range of $range';
+
+  @override
+  String toString() {
+    return 'InvalidValueRangeException($message)';
+  }
+}
+
+/// Inclusive in range checker.
+bool _inRange({
+  required ({num lower, num upper}) range,
+  required num value,
+}) =>
+    value >= range.lower && value <= range.upper;
--- test/utils/asserts_test.dart
+++ test/utils/asserts_test.dart
@@ -1,16 +1,27 @@
 import 'package:gator/gator.dart';
 import 'package:test/test.dart';
 
-import '../test_helpers.dart';
-
 void main() {
   test('assertFactor', () {
-    expect(() => assertFactor(-1), throwsAssertionError);
-    expect(() => assertFactor(101), throwsAssertionError);
+    expect(() => assertFactor(-1), throwsInvalidValueRangeException());
+    expect(() => assertFactor(101), throwsInvalidValueRangeException());
   });
 
   test('assertValue', () {
-    expect(() => assertValue(-1), throwsAssertionError);
-    expect(() => assertValue(256), throwsAssertionError);
+    expect(() => assertValue(-1), throwsInvalidValueRangeException());
+    expect(() => assertValue(256), throwsInvalidValueRangeException());
+  });
+
+  test('InvalidValueRangeException', () {
+    final ex = InvalidValueRangeException(
+      range: (lower: 0, upper: 1),
+      value: 3,
+    );
+    const message = '3 is not in range of (lower: 0, upper: 1)';
+    expect(ex.message, message);
+    expect(ex.toString(), 'InvalidValueRangeException($message)');
   });
 }
+
+Matcher throwsInvalidValueRangeException() =>
+    throwsA(isA<InvalidValueRangeException>());
